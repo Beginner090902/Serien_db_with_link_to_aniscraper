7f1c6729dc8f571d215f61687a8aa701b32329ff
Beginner090902 <milibo45@gmail.com>
2026-03-18 19:48:26 +0100

    bug fix

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
+import get_data_from_website
 from flask import Flask, render_template, redirect, url_for, flash, request, session
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 from werkzeug.exceptions import abort
 from db_manager import DBManager
-import get_data_from_website as get_data 
+from get_data_from_website import add_all_urls_in_table, add_year_real_name_imgage_to_table_in_db, get_all_url_names
 
 db_file="instance/aniworld.db"
 
 table_names = ["anime_namen","serien_namen"]
 websiten_urls = ["https://aniworld.to/animes/","https://s.to/serien/"]
-
+webseiten_einzelneserie_url = ["https://aniworld.to/anime/","https://s.to/serie/"]
 
 def get_db_connection():
     conn = sqlite3.connect('instance/aniworld.db')
@@ -29,13 +30,13 @@ def index():
 def serien_names_sto() -> str:
     db = DBManager(db_file)
     all_anime_urls: tuple = db.get_all_serien_url_from_table(table_name=table_names[1])
-    return render_template('index.html',posts=all_anime_urls)
+    return render_template('serien_names_sto.html',posts=all_anime_urls)
 
 @app.route('/serien_names_aniworld')
 def serien_names_aniworld() -> str:
     db = DBManager(db_file)
     all_anime_urls: tuple = db.get_all_serien_url_from_table(table_name=table_names[0])
-    return render_template('index.html',posts=all_anime_urls)
+    return render_template('serien_names_aniworld.html',posts=all_anime_urls)
 
 @app.route('/<string:such_url>')
 def serie_name(such_url):
@@ -44,17 +45,26 @@ def serie_name(such_url):
     db.close()
     return render_template('serie.html', post=post)
 
-@app.route('/search')
-def search():
+@app.route('/search-aniworld')
+def search_aniworld():
     such_name = request.args.get('such_name', '').lower()
-
     db = DBManager(db_file)
     list_such_name = db.filter_nach_name(
         table_name=table_names[0],
         such_name=such_name
     )
     db.close()
+    return render_template('_search_results.html', posts=list_such_name)
 
+@app.route('/search-sto')
+def search_sto():
+    such_name = request.args.get('such_name', '').lower()
+    db = DBManager(db_file)
+    list_such_name = db.filter_nach_name(
+        table_name=table_names[1],
+        such_name=such_name
+    )
+    db.close()
     return render_template('_search_results.html', posts=list_such_name)
 
 @app.route('/view_database_aniworld')
@@ -74,9 +84,10 @@ def view_database_sto():
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():    
     if request.method == 'POST':
+        
+        
         if 'create_ani_sto_db' in request.form:
             try:
-                db = DBManager(db_file=db_file)
                 for table_name in table_names:
                     result = db.create_table(table_name)
                     flash(result, 'success')
@@ -84,7 +95,20 @@ def settings():
             except Exception as e:
                 flash(f'Fehler beim Erstellen: {str(e)}', 'danger')
             
-            return redirect(url_for('settings'))
+
+        elif 'update_ani_db' in request.form:
+            try:
+                result = add_all_urls_in_table(table_name=table_names[0],list=get_all_url_names(start_url=webseiten_einzelneserie_url[0]))
+                flash(result, "success")
+            except Exception as e:
+                flash(f'Fehler beim Erstellen: {str(e)}', 'danger')
+
+            try:
+                result = add_year_real_name_imgage_to_table_in_db(table_name=table_names[0],start_url=webseiten_einzelneserie_url[0])
+                flash(result, "success")
+            except Exception as e:
+                flash(f'Fehler beim Erstellen: {str(e)}', 'danger')
+                
 
     return render_template('settings.html')
 
--- get_data_from_website.py
+++ get_data_from_website.py
@@ -4,6 +4,7 @@ from db_manager import DBManager
 from tqdm import tqdm
 from urllib.parse import urljoin,urlparse
 import time
+db_file="instance/aniworld.db"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
@@ -42,12 +43,15 @@ def get_all_url_names(start_url):
 
 def add_all_urls_in_table(list:set|list,table_name:str):
     print("Add url to db")
+    db = DBManager(db_file)
     with tqdm(total=len(list)) as pbar:
         for u in list:
             finde_url_in_data_base = db.find_by_title_in_table(table_name=table_name,such_url=u)
             if not finde_url_in_data_base:
                 db.add_such_url_in_table(such_url=u,table_name=table_name)
             pbar.update()
+    db.close()
+    return f"Updated all urls in {table_name}"
 
 def get_year(start_url:str,such_url:str) -> str:
     full_url = f"{start_url}stream/{such_url}"
@@ -55,7 +59,7 @@ def get_year(start_url:str,such_url:str) -> str:
     response.raise_for_status()
     soup = BeautifulSoup(response.text, "html.parser")
     jahr_start = soup.find("span", itemprop="startDate").find("a").text.strip()
-    jahr_ende = soup.find("span", itemprop="startDate").find("a").text.strip()
+    jahr_ende = soup.find("span", itemprop="endDate").find("a").text.strip()
     jahr_zusammen= f"{jahr_start}-{jahr_ende}"
     return jahr_zusammen
 
@@ -78,9 +82,10 @@ def get_image(start_url:str,such_url:str):
     return img_url
 
 def add_year_real_name_imgage_to_table_in_db(start_url:str,table_name:str):
-
+    db = DBManager(db_file)
     list_of_all_anime = db.get_all_in_table(table_name=table_name)
     print("Update all Names ")
+    
     with tqdm(total=len(list_of_all_anime)) as pbar:
         for anime_url in list_of_all_anime:
             #print(anime_url[2])
@@ -88,7 +93,7 @@ def add_year_real_name_imgage_to_table_in_db(start_url:str,table_name:str):
                 anime_name = get_name(start_url=start_url,such_url=anime_url[1])
                 db.add_real_name_on_url_in_table(real_name=anime_name,such_url=anime_url[1],table_name=table_name)
             pbar.update()
-            time.sleep(0.0001)
+            time.sleep(0.001)
             
 
     print("Update all year stamps ")
@@ -99,7 +104,7 @@ def add_year_real_name_imgage_to_table_in_db(start_url:str,table_name:str):
                 anime_year = get_year(start_url=start_url,such_url=anime_url[1])
                 db.add_year_in_table(table_name=table_name,such_url=anime_url[1],year=anime_year)
             pbar.update()
-            time.sleep(0.0001)
+            time.sleep(0.001)
 
     print("Update all Images ")
     with tqdm(total=len(list_of_all_anime)) as pbar:
@@ -109,7 +114,10 @@ def add_year_real_name_imgage_to_table_in_db(start_url:str,table_name:str):
                 anime_img_url = get_image(start_url=start_url,such_url=anime_url[1])
                 db.add_image_in_table(table_name=table_name,such_url=anime_url[1],image_url=anime_img_url)
             pbar.update()
-            time.sleep(0.0001)
+            time.sleep(0.001)
+    db.close()
+
+    return f"Upadated all Names years and image in {table_name} "
 
 
 
